d7947ac3b6145ddce6b2141ab50cd5e85ca9a1ab
rengil <rengil@.mvst.co>
2020-08-14 10:48:14 +0200

    react native test

--- App.js
+++ App.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
+
+const formData = new FormData();
+formData.append('username', 'admin');
+formData.append('password', 'Gil212121');
+
+
+const url = 'http://192.168.1.1/cgi-bin/luci';
 export default function App() {
+
+
+  axios.post(url, formData, { 
+    headers: {
+      'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+      },
+  }).then((response) => {
+    console.log(response);
+  });
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
